c8f33d2b0c720d048f296740b6b644458d2e205d
dostrowski <dostrowski995@gmail.com>
2019-02-21 23:48:02 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker run -t -i --privileged --rm -e DISPLAY=$DISPLAY  --name=dcs_pro dostrowski995/dcs_projekt
-
+xhost +
+sudo docker run -t -i --privileged --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix  --name=dcs_pro dostrowski995/dcs_projekt
